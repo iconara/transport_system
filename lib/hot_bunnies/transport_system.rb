279f1443506c15095c482ad0285f2fdfae8dd5a5
Theo <theo@iconara.net>
2012-01-04 18:08:20 +0100

    Added #setup! and consuming basics

--- lib/hot_bunnies/transport_system.rb
+++ lib/hot_bunnies/transport_system.rb
@@ -9,7 +9,9 @@ module HotBunnies
       @nodes = options[:nodes]
       @connection_factory = options[:connection_factory]
       @exchange_name = options[:exchange_name]
+      @queue_prefix = options[:queue_prefix]
       @exchange_options = options[:exchange_options] || {}
+      @queue_options = options[:queue_options] || {}
       @message_encoder = options[:message_encoder] || Marshal.method(:dump)
       @routing_keys = options[:routing_keys]
       @routing = options[:routing] || method(:random_routing)
@@ -22,15 +24,38 @@ module HotBunnies
       end
     end
 
+    def setup!
+      connect!
+      @exchanges = setup_exchanges!
+      @queues = setup_queues!
+    end
+
     def publisher
       Publisher.new(self)
     end
 
+    def consumer
+      Consumer.new(self)
+    end
+
     def exchanges
-      connect!
-      @connections.map do |connection|
-        channel = connection.create_channel
-        channel.exchange(@exchange_name, @exchange_options.merge(:type => :direct))
+      @exchanges ||= begin
+        connect!
+        @connections.map do |connection|
+          channel = connection.create_channel
+          channel.exchange(@exchange_name, :passive => true)
+        end
+      end
+    end
+
+    def queues
+      @queues ||= begin
+        connect!
+        index_width = [2, Math.log10(@connections.size).to_i + 1].max
+        @connections.each_with_index.map do |connection, i|
+          channel = connection.create_channel
+          channel.queue("#{@queue_prefix}#{i.to_s.rjust(index_width, '0')}", :passive => true)
+        end
       end
     end
 
@@ -47,6 +72,25 @@ module HotBunnies
 
     def random_routing(message)
       rand(@routing_keys.size)
+    end
+
+    def setup_exchanges!
+      @connections.map do |connection|
+        channel = connection.create_channel
+        channel.exchange(@exchange_name, @exchange_options.merge(:type => :direct))
+      end
+    end
+
+    def setup_queues!
+      index_width = [2, Math.log10(@connections.size).to_i + 1].max
+      @connections.each_with_index.map do |connection, i|
+        channel = connection.create_channel
+        queue = channel.queue("#{@queue_prefix}#{i.to_s.rjust(index_width, '0')}", @queue_options)
+        @routing_keys.each_slice(@routing_keys.size/@connections.size).to_a[i].each do |rk|
+          queue.bind(@exchanges[i], :routing_key => rk)
+        end
+        queue
+      end
     end
   end
 
@@ -66,6 +110,22 @@ module HotBunnies
       exchange = @exchanges.sample
       routing_key = @transport_system.select_routing_key(message)
       exchange.publish(encoded_message, :routing_key => routing_key)
+    end
+  end
+
+  class Consumer
+    def initialize(transport_system)
+      @transport_system = transport_system
+      @queues = @transport_system.queues
+    end
+
+    def each(&consumer)
+      @subscriptions = @queues.map { |q| q.subscribe(:ack => true) }
+      @subscriptions.each { |s| s.each(:blocking => false, &consumer) }
+    end
+
+    def stop
+      @subscriptions.each { |s| s.cancel }
     end
   end
 end
--- spec/hot_bunnies/transport_system_spec.rb
+++ spec/hot_bunnies/transport_system_spec.rb
@@ -13,22 +13,34 @@ module HotBunnies
       @connections = []
       @channels = []
       @exchanges = []
+      @queues = []
+      @subscriptions = []
       3.times do |i|
         exchange = stub("exchange#{i}")
+        subscription = stub("subscription#{i}")
+        subscription.stub(:each)
+        queue = stub("queue#{i}")
+        queue.stub(:subscribe).with(:ack => true).and_return(subscription)
+        queue.stub(:bind)
         channel = stub("channel#{i}")
         channel.stub(:exchange).and_return(exchange)
+        channel.stub(:queue).and_return(queue)
+        channel.stub(:close)
         connection = stub("connection#{i}")
         connection.stub(:create_channel).and_return(channel)
         @connection_factory.stub(:connect).with(:uri => "amqp://mqhost0#{i}:5672").and_return(connection)
         @connections << connection
         @channels << channel
         @exchanges << exchange
+        @queues << queue
+        @subscriptions << subscription
       end
+      @routing_keys = %w[r00 r01 r02 r03 r04 r05]
       @options = {
         :connection_factory => @connection_factory,
         :nodes => %w(amqp://mqhost00:5672 amqp://mqhost01:5672 amqp://mqhost02:5672),
         :exchange_name => 'transport_exchange',
-        :routing_keys => %w[r00 r01 r02 r03 r04 r05],
+        :routing_keys => @routing_keys,
         :queue_prefix => 'transport_queue_'
       }
       @transport_system = TransportSystem.new(@options)
@@ -43,6 +55,70 @@ module HotBunnies
       end
     end
 
+    describe '#setup!' do
+      it 'declares the exchange on each node' do
+        @channels.each { |ch| ch.should_receive(:exchange).with('transport_exchange', :type => :direct) }
+        @transport_system.setup!
+      end
+
+      it 'passes along extra exchange options' do
+        @options[:exchange_options] = {:durable => true}
+        @transport_system = TransportSystem.new(@options)
+        @channels.each { |ch| ch.should_receive(:exchange).with('transport_exchange', :type => :direct, :durable => true) }
+        @transport_system.setup!
+      end
+
+      it 'declares a queue on each node' do
+        @channels.each_with_index do |ch, i|
+          ch.should_receive(:queue).with("transport_queue_0#{i}", anything)
+        end
+        @transport_system.setup!
+      end
+
+      it 'passes along extra queue options' do
+        @options[:queue_options] = {:durable => true}
+        @transport_system = TransportSystem.new(@options)
+        @channels.each_with_index do |ch, i|
+          ch.should_receive(:queue).with("transport_queue_0#{i}", :durable => true)
+        end
+        @transport_system.setup!
+      end
+
+      it 'binds the queues to even subsets of the routing keys' do
+        @queues.each_with_index do |q, i|
+          q.should_receive(:bind).with(@exchanges[i], :routing_key => "r#{(i * 2).to_s.rjust(2, '0')}")
+          q.should_receive(:bind).with(@exchanges[i], :routing_key => "r#{(i * 2 + 1).to_s.rjust(2, '0')}")
+        end
+        @transport_system.setup!
+      end
+
+      it 'uses queue suffixes with an appropriate number of leading zeroes', :slow => true do
+        @nodes = []
+        @channels = []
+        222.times do |i|
+          index_str = i.to_s.rjust(3, '0')
+          uri = "amqp://mqhost#{index_str}:5672"
+          exchange = stub("exchange#{index_str}")
+          queue = stub("queue#{index_str}")
+          queue.stub(:bind)
+          channel = stub("channel#{index_str}")
+          channel.stub(:queue).and_return(queue)
+          channel.stub(:exchange).and_return(exchange)
+          connection = stub("connection#{index_str}")
+          connection.stub(:create_channel).and_return(channel)
+          @nodes << uri
+          @channels << channel
+          @connection_factory.stub(:connect).with(:uri => uri).and_return(connection)
+        end
+        @options[:nodes] = @nodes
+        @options[:routing_keys] = 888.times.map { |rk| "r#{rk.to_s.rjust(3, '0')}" }
+        @transport_system = TransportSystem.new(@options)
+        @channels[45].should_receive(:queue).with('transport_queue_045', anything)
+        @channels[111].should_receive(:queue).with('transport_queue_111', anything)
+        @transport_system.setup!
+      end
+    end
+
     context 'publishing' do
       describe '#publisher' do
         it 'returns a publisher' do
@@ -54,15 +130,8 @@ module HotBunnies
           @transport_system.publisher
         end
 
-        it 'declares the exchange on each node' do
-          @channels.each { |ch| ch.should_receive(:exchange).with('transport_exchange', :type => :direct) }
-          @transport_system.publisher
-        end
-
-        it 'passes along extra exchange options' do
-          @options[:exchange_options] = {:durable => true}
-          @transport_system = TransportSystem.new(@options)
-          @channels.each { |ch| ch.should_receive(:exchange).with('transport_exchange', :type => :direct, :durable => true) }
+        it 'passively declares the exchange on each node' do
+          @channels.each { |ch| ch.should_receive(:exchange).with('transport_exchange', :passive => true) }
           @transport_system.publisher
         end
       end
@@ -99,6 +168,50 @@ module HotBunnies
             @publisher = @transport_system.publisher
             @exchanges[1].should_receive(:publish).with('{"id":"ABCDEF"}', :routing_key => 'r03')
             @publisher.publish('id' => 'ABCDEF')
+          end
+        end
+      end
+    end
+
+    context 'consuming' do
+      describe '#consumer' do
+        it 'returns a consumer' do
+          @transport_system.consumer.should_not be_nil
+        end
+
+        it 'creates a channel on each node' do
+          @connections.each { |c| c.should_receive(:create_channel) }
+          @transport_system.consumer
+        end
+
+        it 'passively declares a queue on each node' do
+          @channels.each_with_index do |ch, i|
+            ch.should_receive(:queue).with("transport_queue_0#{i}", :passive => true)
+          end
+          @transport_system.consumer
+        end
+      end
+
+      describe Consumer do
+        before do
+          @consumer = @transport_system.consumer
+        end
+
+        describe '#each' do
+          it 'subscribes to each queue' do
+            received_value = nil
+            block = proc { |h, m| received_value = [h, m] }
+            @subscriptions.each { |s| s.should_receive(:each) { |options, &callback| callback.call('test', 'tset') } }
+            @consumer.each(&block)
+            received_value.should == ['test', 'tset']
+          end
+        end
+
+        describe '#stop' do
+          it 'cancels all subscriptions' do
+            @subscriptions.each { |s| s.should_receive(:cancel) }
+            @consumer.each { |m| }
+            @consumer.stop
           end
         end
       end
